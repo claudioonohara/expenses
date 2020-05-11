f7b491a51a8aeee9e27a6deee9a58c89e5844b7d
Claudio Toyoshi Onohara <claudioonohara@gmail.com>
2020-05-11 07:23:34 -0400

    iniciado projeto com scaffold

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyhomePage());
+  }
+}
+
+class MyhomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+      ),
+      body: Center(child: Text('Versão Inicial'),),
+    );
+  }
+}
